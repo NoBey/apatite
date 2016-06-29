520aa26fd880a75de0ae9573ab78e128d62f2d71
pom <pmadhur@hotmail.com>
2016-06-29 18:30:40 +0200

    fix typo, write more test cases

--- test/mapping/apatite-simple-mapping-test.js
+++ test/mapping/apatite-simple-mapping-test.js
@@ -5,6 +5,7 @@ var expect = require('chai').expect;
 
 var ApatiteTestUtil = require('../apatite-test-util.js');
 var util = new ApatiteTestUtil();
+var ApatiteOneToOneProxy = require('../../lib/model/apatite-one-to-one-proxy.js');
 
 describe('ApatiteSimpleMappingTest', function() {
     it('Simple Mapping Validity', function () {
@@ -31,7 +32,15 @@ describe('ApatiteSimpleMappingTest', function() {
         modelDescriptor.newSimpleMapping('id', column);
         
         column = table.addNewColumn('NAME', apatite.dialect.newVarCharType(100));
-        modelDescriptor.newSimpleMapping('name', column);
+        var mapping = modelDescriptor.newSimpleMapping('name', column);
+        var value = mapping.getAttrValueFromObject(undefined, 'foo');
+        expect(value).to.equal(null);
+        var proxy = new ApatiteOneToOneProxy(null);
+        value = mapping.getAttrValueFromObject({'foo': proxy}, 'foo');
+        expect(value).to.equal(null);
+        proxy.setValue('test');
+        value = mapping.getAttrValueFromObject({'foo': proxy}, 'foo');
+        expect(value).to.equal('test');
 
         column = table.addNewColumn('FOO', apatite.dialect.newVarCharType(100));
         
