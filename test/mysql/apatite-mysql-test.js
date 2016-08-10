6b319b8fdb2f5484193a23283b42aea425369e72
pom <pmadhur@hotmail.com>
2016-08-10 12:49:34 +0200

    fix mysql failing test

--- test/mysql/apatite-mysql-test.js
+++ test/mysql/apatite-mysql-test.js
@@ -10,6 +10,7 @@ describe('ApatiteMysqlTest', function () {
 
         after(function (done) {
             helper.tearDown(done, util, session);
+            doPoolTest();
         });
 
         it('Mysql Validity', function (done) {
@@ -18,42 +19,47 @@ describe('ApatiteMysqlTest', function () {
     }
 })
 
-describe('ApatiteMysqlPoolTest', function () {
-    var ApatiteMysqlTestUtil = require('./apatite-mysql-test-util');
-    var util = new ApatiteMysqlTestUtil();
-    if (util.existsModule()) {
-        var helper = require('../apatite-dialect-pool-test-helper.js');
-        var session = null;
-        before(function (done) {
-            helper.setUp(done, util, function (sess) { session = sess; });
-        });
+function doPoolTest() {
+    describe('ApatiteMysqlPoolTest', function () {
+        var ApatiteMysqlTestUtil = require('./apatite-mysql-test-util');
+        var util = new ApatiteMysqlTestUtil();
+        if (util.existsModule()) {
+            var helper = require('../apatite-dialect-pool-test-helper.js');
+            var session = null;
+            before(function (done) {
+                helper.setUp(done, util, function (sess) { session = sess; });
+            });
 
-        after(function (done) {
-            helper.tearDown(done, util, session);
-        });
+            after(function (done) {
+                helper.tearDown(done, util, session);
+                doPoolErrorTest();
+            });
 
-        it('Mysql Connection Pool Validity', function (done) {
-            helper.testFunction(done, session, util);
-        });
-    }
-})
+            it('Mysql Connection Pool Validity', function (done) {
+                helper.testFunction(done, session, util);
+            });
+        }
+    })    
+}
 
-describe('ApatiteMysqlPoolErrorTest', function () {
-    var ApatiteMysqlTestUtil = require('./apatite-mysql-test-util');
-    var util = new ApatiteMysqlTestUtil();
-    if (util.existsModule()) {
-        var helper = require('../apatite-dialect-pool-error-test-helper.js');
-        var session = null;
-        before(function (done) {
-            helper.setUp(done, util, function (sess) { session = sess; });
-        });
+function doPoolErrorTest() {
+    describe('ApatiteMysqlPoolErrorTest', function () {
+        var ApatiteMysqlTestUtil = require('./apatite-mysql-test-util');
+        var util = new ApatiteMysqlTestUtil();
+        if (util.existsModule()) {
+            var helper = require('../apatite-dialect-pool-error-test-helper.js');
+            var session = null;
+            before(function (done) {
+                helper.setUp(done, util, function (sess) { session = sess; });
+            });
 
-        after(function (done) {
-            helper.tearDown(done, util, session);
-        });
+            after(function (done) {
+                helper.tearDown(done, util, session);
+            });
 
-        it('Mysql Connection Pool Error Validity', function (done) {
-            helper.testFunction(done, session, util);
-        });
-    }
-})
+            it('Mysql Connection Pool Error Validity', function (done) {
+                helper.testFunction(done, session, util);
+            });
+        }
+    })
+}
